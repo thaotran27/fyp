8a6d192ae6bfd8cb93232b9cfc16349fe5dd7e88
YuHaihong <77481132+itshaihong@users.noreply.github.com>
2023-11-06 22:26:48 +0800

    keep to date

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,20 +2,44 @@
 from joblib import load
 import json
 from pathlib import Path
-
+import numpy as np
 from sklearn.metrics import accuracy_score
-
 from train import load_data
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import metrics
+
+
+    
 
 def main(repo_path):
     test_csv_path = repo_path / "data/prepared/test.csv"
     test_data, labels = load_data(test_csv_path)
-    model = load(repo_path / "model/model.joblib")
-    predictions = model.predict(test_data)
-    accuracy = accuracy_score(labels, predictions)
-    metrics = {"accuracy": accuracy}
-    accuracy_path = repo_path / "metrics/accuracy.json"
-    accuracy_path.write_text(json.dumps(metrics))
+    model_RF = load(repo_path / "model/modelRF.joblib")
+    model_XGB = load(repo_path / "model/modelXGB.joblib")
+
+    predictions_RF = model_RF.predict(test_data)
+    accuracy_RF = accuracy_score(labels, predictions_RF)
+    metrics_RF = {"accuracy": accuracy_RF}
+    accuracy_path_RF = repo_path / "metrics/accuracy_RF.json"
+    accuracy_path_RF.write_text(json.dumps(metrics_RF))
+    disp1 = metrics.ConfusionMatrixDisplay.from_predictions(labels, predictions_RF)
+
+    predictions_XGB = model_XGB.predict(test_data)
+    classmap = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'O', 5: 'S'}
+    predictions_XGB_decoded = np.array([classmap[value] for value in predictions_XGB])
+    accuracy_XGB = accuracy_score(labels, predictions_XGB_decoded)
+    metrics_XGB = {"accuracy": accuracy_XGB}
+    accuracy_path_XGB = repo_path / "metrics/accuracy_XGB.json"
+    accuracy_path_XGB.write_text(json.dumps(metrics_XGB))
+    disp2 = metrics.ConfusionMatrixDisplay.from_predictions(labels, predictions_XGB_decoded)
+
+    f, axes = plt.subplots(1, 2, figsize=(20, 5), sharey='row')
+    disp1.plot(ax=axes[0], xticks_rotation=45)
+    disp1.ax_.set_title("RandomForest")
+    disp1.plot(ax=axes[1], xticks_rotation=45)
+    disp1.ax_.set_title("XGBoost")
+    plt.show()
 
 if __name__ == "__main__":
     repo_path = Path(__file__).parent.parent
--- src/train.py
+++ src/train.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+import sys
 from sklearn.metrics import accuracy_score
 
 def load_data(data_path):
@@ -14,19 +16,30 @@ def load_data(data_path):
     y = dataset_train["label"].to_numpy()
     return X, y
 
-def get_model(X_train, y_train):
+def get_model(X_train, y_train, model_name):
     print("start training")
-    clf = RandomForestClassifier(max_depth=2, random_state=0)
-    clf.fit(X_train, y_train)
+    print(pd.DataFrame(X_train))
+    if(model_name == "RandomForest"):
+        clf = RandomForestClassifier(max_depth=2, random_state=0)
+        clf.fit(X_train, y_train)
+    if(model_name == "XGBoost"):
+        classmap = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'O', 5: 'S'}
+        reverse_classmap = {v: k for k, v in classmap.items()}
+        y_train_encoded = np.array([reverse_classmap[value] for value in y_train])
+        clf = XGBClassifier(n_estimators=100, max_depth=5, learning_rate=0.1)
+        clf.fit(X_train, y_train_encoded)
     return clf
 
 def main(repo_path):
     train_csv_path = repo_path / "data/prepared/train.csv"
     X_train, y_train = load_data(train_csv_path)
-    classifier = get_model(X_train, y_train)
+    model_name = sys.argv[1]
+    classifier = get_model(X_train, y_train, model_name)
     print(repo_path)
-    dump(classifier, repo_path / "model/model.joblib")
-    
+    if(model_name == "RandomForest"):
+        dump(classifier, repo_path / "model/modelRF.joblib")
+    if(model_name == "XGBoost"):
+        dump(classifier, repo_path / "model/modelXGB.joblib")
     '''
     train_csv_path = repo_path / "data/prepared/train.csv"
     train_data, labels = load_data(train_csv_path)
