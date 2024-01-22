355fc9b976637ac2ed1c21b67db15810fc3d805d
itshaihong <haihongyu28@gmail.com>
2024-01-22 11:49:08 +0800

    update more letters on training testing

--- src/evaluate.py
+++ src/evaluate.py
@@ -8,15 +8,22 @@ from train import load_data
 import seaborn as sns
 from sklearn import metrics
 
-
+classmap = {0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 
+            5:'F', 6:'G', 7:'H', 8:'I', 9:'J',
+            10:'K', 11:'L', 12:'M', 13:'N', 14:'O', 
+            15:'P', 16:'Q', 17:'R', 18:'S', 19:'T', 
+            20:'U', 21:'V', 22:'W', 23:'X', 24:'Y', 25:'Z'}
     
 
 def main(repo_path):
     test_csv_path = repo_path / "data/prepared/test.csv"
     test_data, labels = load_data(test_csv_path)
+
+    # load models
     model_RF = load(repo_path / "model/modelRF.joblib")
     model_XGB = load(repo_path / "model/modelXGB.joblib")
 
+    # prediction and evaluation of RF
     predictions_RF = model_RF.predict(test_data)
     metrics_RF = {'Accuracy': accuracy_score(labels, predictions_RF), 
                   'f1-score': f1_score(labels, predictions_RF, average='weighted'), 
@@ -25,9 +32,8 @@ def main(repo_path):
     metrics_path_RF = repo_path / "metrics/metrics_RF.json"
     metrics_path_RF.write_text(json.dumps(metrics_RF))
     
-
+    # prediction and evaluation of XGBoost
     predictions_XGB = model_XGB.predict(test_data)
-    classmap = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'O', 5: 'S'}
     predictions_XGB_decoded = np.array([classmap[value] for value in predictions_XGB])
     metrics_XGB = {'Accuracy': accuracy_score(labels, predictions_XGB_decoded), 
                   'f1-score': f1_score(labels, predictions_XGB_decoded, average='weighted'), 
@@ -37,6 +43,7 @@ def main(repo_path):
     metrics_path_XGB.write_text(json.dumps(metrics_XGB))
     
 
+    # display heatmap
     disp1 = metrics.ConfusionMatrixDisplay.from_predictions(labels, predictions_RF)
     disp1.figure_.savefig('confusion_matrix/confusion_matrix_RF.png')
     disp2 = metrics.ConfusionMatrixDisplay.from_predictions(labels, predictions_XGB_decoded)
--- src/train.py
+++ src/train.py
@@ -10,6 +10,14 @@ from xgboost import XGBClassifier
 import sys
 from micromlgen import port
 
+# some models e.g.XGBoost need integer index as label
+classmap = {0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 
+            5:'F', 6:'G', 7:'H', 8:'I', 9:'J',
+            10:'K', 11:'L', 12:'M', 13:'N', 14:'O', 
+            15:'P', 16:'Q', 17:'R', 18:'S', 19:'T', 
+            20:'U', 21:'V', 22:'W', 23:'X', 24:'Y', 25:'Z'}
+reverse_classmap = {v: k for k, v in classmap.items()}
+
 def load_data(data_path):
     dataset_train = pd.read_csv(data_path)
     X = dataset_train[["Thumb","Index","Middle","Ring","Pinky","ax","ay","az","gx","gy","gz"]].to_numpy()
@@ -17,14 +25,13 @@ def load_data(data_path):
     return X, y
 
 def get_model(X_train, y_train, model_name):
-    print("start training")
-    print(pd.DataFrame(X_train))
+    print("start training, model name:", model_name)
+
     if(model_name == "RandomForest"):
-        clf = RandomForestClassifier(n_estimators=100, max_depth=2, criterion='gini', random_state=0)
+        clf = RandomForestClassifier(n_estimators=100, max_depth=5, criterion='gini', random_state=0)
         clf.fit(X_train, y_train)
+
     if(model_name == "XGBoost"):
-        classmap = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'O', 5: 'S'}
-        reverse_classmap = {v: k for k, v in classmap.items()}
         y_train_encoded = np.array([reverse_classmap[value] for value in y_train])
         clf = XGBClassifier(n_estimators=100, max_depth=5, learning_rate=0.1)
         clf.fit(X_train, y_train_encoded)
@@ -37,18 +44,21 @@ def main(repo_path):
     classifier = get_model(X_train, y_train, model_name)
     print(repo_path)
 
-
-    classmap = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'O', 5: 'S'}
     c_code_path = repo_path / "cheader"
-    c_code = port(classifier, classmap=classmap, tmp_file = c_code_path / 'xgboost.json')
+    c_code = port(classifier, classmap=classmap)
 
     if(model_name == "RandomForest"):
+        # store model in pipeline
         dump(classifier, repo_path / "model/modelRF.joblib")
+        # output header file for model
         f = open(c_code_path / "RandomForest.h", "w")
         f.write(c_code)
         f.close()
+
     if(model_name == "XGBoost"):
+        # store model in pipeline
         dump(classifier, repo_path / "model/modelXGB.joblib")
+        # output header file for model
         f = open( c_code_path / "XGBoost.h", "w")
         f.write(c_code)
         f.close()
