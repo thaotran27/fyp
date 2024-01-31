354d1dc860836e9de84218632abca2d7a458d230
thaotran27 <tranthaoptcnn@gmail.com>
2024-01-31 15:13:22 +0800

    train with only stevens data

--- src/train.py
+++ src/train.py
@@ -36,7 +36,7 @@ def get_model(X_train, y_train, model_name):
 
     if(model_name == "XGBoost"):
         y_train_encoded = np.array([reverse_classmap[value] for value in y_train])
-        clf = XGBClassifier(n_estimators=50, max_depth=5, learning_rate=0.1)
+        clf = XGBClassifier(n_estimators=20, max_depth=5, learning_rate=0.1)
         clf.fit(X_train, y_train_encoded)
     return clf
 
@@ -47,7 +47,8 @@ def main(repo_path):
     classifier = get_model(X_train, y_train, model_name)
     print(repo_path)
 
-    c_code_path = repo_path / "cheader"
+    #c_code_path = repo_path / "cheader"
+    c_code_path = repo_path / "Hardware" / "Prediction"
     c_code = port(classifier, classmap=classmap, tmp_file = c_code_path / 'xgboost.json')
 
     if(model_name == "RandomForest"):
