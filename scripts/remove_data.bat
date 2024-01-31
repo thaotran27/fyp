d2173c5452d3aaa9af8a7efb98614e206c2c30d0
thaotran27 <tranthaoptcnn@gmail.com>
2024-01-31 15:07:23 +0800

    add data files

---
+++ scripts/remove_data.bat
@@ -0,0 +1,32 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "base_path=./data/data_from_PC/Newdata/Newdata"
+set "script_path=./src/clean.py"
+set "output_dir=./data/raw"
+set "log_file=scripts/remove_data_error_log.txt"
+
+:: Define a list of words to process
+set "word_list=a b c d e f g h i j k l m n o p q r s t u v w x y z"
+
+for %%w in (%word_list%) do (
+    set "current_word=%%w"
+    set "input_file=!base_path!/!current_word!.csv"
+    
+    if exist "!input_file!" (
+        dvc remove "!output_dir!/!current_word!.csv.dvc" || (
+            echo Error removing DVC file for !current_word! >> "!log_file!"
+        )
+        dvc remove "!output_dir!/!current_word!.csv" || (
+            echo Error removing DVC file for !current_word! >> "!log_file!"
+        )
+    )
+)
+
+if exist "!log_file!" (
+    echo Errors occurred. Check "!log_file!" for details.
+) else (
+    echo Done!
+)
+
+exit /b 0
