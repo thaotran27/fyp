21a26bc78f5d8b86354dc11172fd302347eb12fd
thaotran27 <tranthaoptcnn@gmail.com>
2024-02-21 13:24:20 +0800

    remove data files

--- scripts/add_data.bat
+++ scripts/add_data.bat
@@ -6,22 +6,23 @@ set "script_path=./src/clean.py"
 set "output_dir=./data/raw"
 set "log_file=scripts/add_data_error_log.txt"
 
-:: Define a list of words to process
-set "word_list=a b c d e f g h i j k l m n o p q r s t u v w x y z"
-
-for %%w in (%word_list%) do (
-    set "current_word=%%w"
-    set "input_file=!base_path!/!current_word!.csv"
-    
-    if exist "!input_file!" (
-        python "!script_path!" "!input_file!" !current_word! || (
-            echo Error processing !current_word! >> "!log_file!"
-        )
-        dvc add "!output_dir!/!current_word!.csv" || (
-            echo Error adding !current_word! to DVC >> "!log_file!"
-        )
-        git add "!output_dir!/!current_word!.csv.dvc" || (
-            echo Error adding DVC file for !current_word! to Git >> "!log_file!"
+:: Iterate through files in the directory
+for %%F in ("%base_path%\*") do (
+    set "full_file=%%~nxF"
+    for %%A in ("!full_file:~0,1!") do (
+        set "current_word=%%~xA"
+        set "input_file=!base_path!\!current_word!"
+        
+        if exist "!input_file!" (
+            python "!script_path!" "!input_file!" !current_word! || (
+                echo Error processing !current_word! >> "!log_file!"
+            )
+            dvc add "!output_dir!\!current_word!" || (
+                echo Error adding !current_word! to DVC >> "!log_file!"
+            )
+            git add "!output_dir!\!current_word!.dvc" || (
+                echo Error adding DVC file for !current_word! to Git >> "!log_file!"
+            )
         )
     )
 )
