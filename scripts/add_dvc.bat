6ff248f0addd5a5550d71665cd719f8f2090f349
thaotran27 <tranthaoptcnn@gmail.com>
2024-02-28 16:58:26 +0800

    add add_dvc script, to make experimenting easier

--- scripts/add_dvc.bat
+++ scripts/add_dvc.bat
@@ -1,10 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "base_path=../steven/steven"
-set "script_path=./src/clean.py"
-set "output_dir=./data/raw"
-set "log_file=scripts/add_data_error_log.txt"
+set "base_path=./data/raw"
+set "log_file=scripts/add_dvc_err_log.txt"
 
 :: Iterate through files in the directory
 for %%F in ("%base_path%\*") do (
@@ -19,17 +17,13 @@ for %%F in ("%base_path%\*") do (
             )
         )
         echo Processing word: !current_word!
-        set "input_file=!base_path!\!full_file!"
         
-        python "!script_path!" "!input_file!" !current_word! || (
-            echo Error processing !current_word! >> "!log_file!"
+        dvc add "!base_path!\!current_word!.csv" || (
+            echo Error adding !current_word! to DVC >> "!log_file!"
         )
-        :: dvc add "!output_dir!\!current_word!.csv" || (
-        ::    echo Error adding !current_word! to DVC >> "!log_file!"
-        ::)
-        ::git add "!output_dir!\!current_word!.csv.dvc" || (
-        ::    echo Error adding DVC file for !current_word! to Git >> "!log_file!"
-        ::)
+        git add "!base_path!\!current_word!.csv.dvc" || (
+            echo Error adding DVC file for !current_word! to Git >> "!log_file!"
+        )
     )
 )
 
